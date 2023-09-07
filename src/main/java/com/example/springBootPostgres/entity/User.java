f126042e564ca196f99ed1ff8d2155c407c007e3
Admin <Evgeny.Grand@mail.ru>
2023-09-07 23:57:32 +0300

    deleteAll

--- src/main/java/com/example/springBootPostgres/entity/User.java
+++ src/main/java/com/example/springBootPostgres/entity/User.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 @Table(name = "users")
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
     @Column(name = "name", nullable = false)
--- src/test/java/com/example/springBootPostgres/SpringBootPostgresApplicationTests.java
+++ src/test/java/com/example/springBootPostgres/SpringBootPostgresApplicationTests.java
@@ -7,6 +7,7 @@ import com.example.springBootPostgres.entity.User;
 import com.example.springBootPostgres.repository.UserRepository;
 import io.restassured.response.Response;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,7 +20,8 @@ import static io.restassured.RestAssured.given;
 
 class SpringBootPostgresApplicationTests {
 	private final static String URL = "http://localhost:8080";
-	private final static String APIUSERS ="/users/";
+	private final static String API_USERS ="/users/";
+	private final static String API_USERS_ID = API_USERS+"1";
 
 	@Autowired
 	UserRepository userRepository;
@@ -27,27 +29,66 @@ class SpringBootPostgresApplicationTests {
 	@Test
 	public void createEmployeeTest() {
 		Specification.installSpecification(Specification.requestSpec(URL),Specification.responceSpecOk200());
-		UserDto userDTO = new UserDto(4, "Sibilla", "test@test.ru");
+		UserDto userDTO = new UserDto(1, "Anna", "Anna@test.ru");
 		Response response = given()
 				.body(userDTO)
 				.when()
-				.post(APIUSERS).
+				.post(API_USERS).
 				then().log().all()
 				.extract().response();
 				List<User> users = userRepository.findAll();
 		        Assertions.assertThat(users.size()).isGreaterThan(0);
 
+	}
+
+	@Test
+	public void findUserById(){
+		Specification.installSpecification(Specification.requestSpec(URL),Specification.responceSpecOk200());
+		Response response = given()
+				.with()
+				.get(API_USERS_ID)
+				.then().log().all()
+				.extract().response();
+		User user = userRepository.findByName("Anna").get();
+		Assertions.assertThat(user.getName()).isEqualTo("Anna");
+		Assertions.assertThat(user.getEmail()).endsWith("@test.ru");
+	}
 
 
+	@Test
+	public void updateUserTest(){
+		Specification.installSpecification(Specification.requestSpec(URL),Specification.responceSpecOk200());
+		UserDto userDTO = new UserDto(1, "Elena", "Elena@test.ru");
+		Response response = given()
+				.body(userDTO)
+				.when()
+				.put(API_USERS).
+				then().log().all()
+				.extract().response();
+		User user = userRepository.findByName("Anna").get();
+		Assertions.assertThat(user.getName()).isEqualTo("Elena");
+		Assertions.assertThat(user.getEmail()).startsWith("Elena");
+	}
+
+
+	@Test
+	public void deleteUser(){
+		Specification.installSpecification(Specification.requestSpec(URL),Specification.responceSpecOk200());
+		Response response = given()
+				.with()
+				.delete(API_USERS_ID)
+				.then().log().all()
+				.extract().response();
+		User user = userRepository.findByName("Elena").get();
+		Assertions.assertThat(user.getName()).isNull();
 
 	}
-//	@Test
-//    public void checkIDsTest(){
-//		List<User> users = userRepository.findAll();
-//		Assertions.assertThat(users.size()).isGreaterThan(0);
-//
-//	}
 
+
+	@Test
+	public void cleanDB(){
+		userRepository.deleteAll();
+	}
 
 
 }
